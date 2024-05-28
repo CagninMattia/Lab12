4c0dc62f1a3238d19ef60f86ee3ae626fc533e9d
CagninMattia <s297196@studenti.polito.it>
2024-05-28 15:59:16 +0200

    fatto

--- UI/controller.py
+++ UI/controller.py
@@ -12,17 +12,34 @@ class Controller:
         self._listCountry = []
 
     def fillDD(self):
-        pass
+        nazioni = self._model.get_nazioni()
+        for c in nazioni:
+            self._view.ddcountry.options.append(ft.dropdown.Option(c))
 
 
     def handle_graph(self, e):
-        pass
+        anno = int(self._view.ddyear.value)
+        nazione = self._view.ddcountry.value
+        self._model.crea_grafo(nazione, anno)
+        num_nodi = self._model.num_nodi()
+        num_archi = self._model.num_archi()
+        self._view.txt_result.controls.append(ft.Text(f"Numero nodi: {num_nodi}"))
+        self._view.txt_result.controls.append(ft.Text(f"Numero archi: {num_archi}"))
+        self._view.update_page()
 
 
 
     def handle_volume(self, e):
-        pass
+        diz = self._model.volume_vendita()
+        for retailer, volume in diz.items():
+            self._view.txtOut2.controls.append(ft.Text(f"{retailer} --> {volume}"))
+        self._view.update_page()
 
 
     def handle_path(self, e):
-        pass
+        numero = int(self._view.txtN.value)
+        costo, ciclo = self._model.get_ciclo_max(numero)
+        print(costo)
+        for n in ciclo:
+            print(n)
+
